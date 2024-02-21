0bb7a26355ad43133cf4527ddbba03106bcd767c
Tom Pierce <tompierce4@gmail.com>
2024-02-21 13:47:40 +0000

    user signup and login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,68 @@
+import { useState } from "react";
+import styles from "../css/AuthenticationForm.module.css";
+
 const Login = () => {
-  return <>Login</>;
+  const [errorMsg, setErrorMsg] = useState(undefined);
+
+  const localClickHandler = async (e) => {
+    e.preventDefault();
+    const form = document.getElementById("loginForm");
+    const email = form.elements["email"].value;
+    const password = form.elements["password"].value;
+
+    const res = await fetch(
+      `${import.meta.env.VITE_API_URL}api/auth/local/login`,
+      {
+        method: "post",
+        mode: "cors",
+        body: JSON.stringify({ email, password }),
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (res.status === 200)
+      window.location.href = `${window.location.protocol}//${window.location.host}/`;
+
+    if (res.status === 401) {
+      const json = await res.json();
+      setErrorMsg(json.message);
+    }
+  };
+
+  return (
+    <div className="main">
+      <form id="loginForm" className={`box ${styles.authenticationForm}`}>
+        <input
+          type="email"
+          id="emailInput"
+          name="email"
+          className={`${styles.textInput} textInput`}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          id="passwordInput"
+          name="password"
+          className={`${styles.textInput} textInput`}
+          placeholder="Password"
+        />
+
+        {errorMsg ? <p>{errorMsg}</p> : null}
+
+        <div>
+          <button
+            className={`${styles.btn} btn`}
+            type="submit"
+            onClick={localClickHandler}
+          >
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 export default Login;
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,4 +1,85 @@
+import { useState } from "react";
+import styles from "../css/AuthenticationForm.module.css";
+
 const SignUp = () => {
-  return <>SIGNUP</>;
+  const [errorMsgs, setErrorMsgs] = useState([]);
+
+  const localClickHandler = async (e) => {
+    e.preventDefault();
+    const form = document.getElementById("signUpForm");
+    const email = form.elements["email"].value;
+    const password = form.elements["password"].value;
+    const confirmPassword = form.elements["confirmPassword"].value;
+
+    const res = await fetch(
+      `${import.meta.env.VITE_API_URL}api/auth/local/signup`,
+      {
+        method: "post",
+        mode: "cors",
+        body: JSON.stringify({ email, password, confirmPassword }),
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (res.status === 201)
+      window.location.href = `${window.location.protocol}//${window.location.host}/`;
+
+    if (res.status === 400) {
+      const json = await res.json();
+      setErrorMsgs(json);
+    }
+  };
+
+  return (
+    <div className="main">
+      <form id="signUpForm" className={`box ${styles.authenticationForm}`}>
+        <input
+          type="email"
+          id="emailInput"
+          name="email"
+          className={`${styles.textInput} textInput`}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          id="passwordInput"
+          name="password"
+          className={`${styles.textInput} textInput`}
+          placeholder="Password"
+        />
+        <input
+          type="password"
+          id="confirmPasswordInput"
+          name="confirmPassword"
+          className={`${styles.textInput} textInput`}
+          placeholder="Confirm Password"
+        />
+        {errorMsgs.length !== 0 ? (
+          <ul>
+            {errorMsgs.map((msg, index) => {
+              return (
+                <li className={styles.errorMessage} key={`error-msg-${index}`}>
+                  {msg}
+                </li>
+              );
+            })}
+          </ul>
+        ) : null}
+
+        <div>
+          <button
+            className={`${styles.btn} btn`}
+            type="submit"
+            onClick={localClickHandler}
+          >
+            Sign up
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 export default SignUp;
