61bbe32dfe4ffad6af5a031877ccf78951f4ba6f
Tom Pierce <tompierce4@gmail.com>
2024-02-22 12:49:16 +0000

    basic table of each position

--- src/components/Positions.jsx
+++ src/components/Positions.jsx
@@ -1,8 +1,57 @@
 import { useContext } from "react";
 import { UserContext } from "../App";
+import styles from "../css/Positions.module.css";
 
 const Positions = () => {
   const { userPortfolio } = useContext(UserContext);
   console.log(userPortfolio);
+  return (
+    <>
+      {userPortfolio ? (
+        <div className={styles.positionsTable}>
+          <ul>
+            <li className={styles.row}>
+              <div className={styles.cell}>
+                <p>ticker</p>
+              </div>
+              <div className={styles.cell}>
+                <p>value</p>
+              </div>
+              <div className={styles.cell}>
+                <p>current price</p>
+              </div>
+              <div className={styles.cell}>
+                <p>total cost</p>
+              </div>
+              <div className={styles.cell}>
+                <p>diff</p>
+              </div>
+            </li>
+            {userPortfolio.positions.map((position) => {
+              return (
+                <li key={`${position.ticker}-row`} className={styles.row}>
+                  <div className={styles.cell}>
+                    <p>{position.ticker}</p>
+                  </div>
+                  <div className={styles.cell}>
+                    <p>{position.value}</p>
+                  </div>
+                  <div className={styles.cell}>
+                    <p>{position.currentPrice}</p>
+                  </div>
+                  <div className={styles.cell}>
+                    <p>{position.cost}</p>
+                  </div>
+                  <div className={styles.cell}>
+                    <p>{position.value - position.cost}</p>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      ) : null}
+    </>
+  );
 };
 export default Positions;
