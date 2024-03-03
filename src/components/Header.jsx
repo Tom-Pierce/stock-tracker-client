c7c69dcab7cb8a68c6f0871f8dc69da1d38fea9e
Tom Pierce <tompierce4@gmail.com>
2024-03-03 12:59:12 +0000

    login and logout button in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,49 @@
+import { useContext } from "react";
 import styles from "../css/Header.module.css";
+import { UserContext } from "../App";
+import { Link, Navigate } from "react-router-dom";
 
 const Header = () => {
+  const { userPortfolio, setUserPortfolio } = useContext(UserContext);
+
+  const logout = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/auth/local/logout`,
+        {
+          method: "POST",
+          mode: "cors",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (res.status === 200) {
+        setUserPortfolio(undefined);
+      }
+
+      <Navigate to="/" replace={true} />;
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
+
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>StockTracker</h1>
+
+      {userPortfolio ? (
+        <button className={styles.logButton} onClick={logout}>
+          log out
+        </button>
+      ) : (
+        <Link className={styles.logButton} to="/login" replace={true}>
+          login
+        </Link>
+      )}
     </header>
   );
 };
